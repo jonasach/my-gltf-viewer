d69780f76db8e55f96dfcdebd09b4553703fa634
jonasach <jonasach@gmail.com>
2023-07-20 10:35:14 -0400

    07.20.2023 AM

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
+Copy code
+#!/bin/bash
+
+# Get the current date and time in the desired format
+timestamp=$(date +"%m.%d.%Y %p")
+
+# Add all changes to the staging area
 git add .
-git commit -m "06.25.2023 AM"
+
+# Commit the changes with the dynamically generated timestamp
+git commit -m "$timestamp"
+
+# Push the changes to the remote repository
 git push origin main
+
+
